b36dd45b0ae7b7d256be4a77fb919e1866af8e92
Anderson ARruda <andmarruda@gmail.com>
2022-07-16 23:17:48 -0300

    Building admin template

--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -5,6 +5,42 @@
         <nav class="navbar navbar-expand-lg navbar-light bglight">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">SBShowcase</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#admin-navbar" aria-controls="admin-navbar" aria-expanded="false">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="admin-navbar">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pedidos</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Produto
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{route('category')}}">Categoria</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Configurações
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Template</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sair</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         
@@ -13,5 +49,7 @@
                 @yield('page')
             </div>
         </div>
+
+        <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
     </body>
 </html>
--- resources/views/template/includes/head.blade.php
+++ resources/views/template/includes/head.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/sbshowcase.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/sbshowcase.css')}}">
 </head>
