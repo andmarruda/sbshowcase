aef0c6ae5ec902197ba75d35bb3e26b1ed39feed
Anderson ARruda <andmarruda@gmail.com>
2022-07-18 11:50:59 -0300

    Preparing admin layout

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -1,5 +1,25 @@
 @extends('template.admin')
 
 @section('page')
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Produto</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Categoria</li>
+    </ol>
+</nav>
 
+<form method="post" action="" autocomplete="off">
+    <input type="hidden" name="id" id="id" value="">
+    @csrf
+
+    <div class="mb-3">
+        <label for="category" class="form-label">Nome da categoria</label>
+        <input type="text" maxlength="100" class="form-control" id="category" name="category" placeholder="Nome da categoria" required value="">
+    </div>
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary"><i class="fa-regular fa-floppy-disk"></i> Salvar</button>
+        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#modalSearchCategory">Pesquisar</button>
+    </div>
+</form>
 @endsection
--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -1,53 +1,69 @@
 <!DOCTYPE html>
 <html>
     @include('template.includes.head', ['title' => 'SBShowcase - Admin'])
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light bglight">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">SBShowcase</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#admin-navbar" aria-controls="admin-navbar" aria-expanded="false">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="admin-navbar">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pedidos</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Produto
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="{{route('category')}}">Categoria</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Configurações
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Template</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Sair</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
+    <body class="body-admin">
         <div class="container-lg">
             <div class="row">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">SBShowcase</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#admin-navbar" aria-controls="admin-navbar" aria-expanded="false">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="admin-navbar">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Pedidos</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Produto
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="{{route('category')}}">Categoria</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Configurações
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="#">Template</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Sair</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="row admin-content">
                 @yield('page')
             </div>
+
+            <footer class="row">
+                <div class="d-flex justify-content-between align-items-center">
+                    Todos os direitos reservados. © 2021-2031
+
+                    <div>
+                        SBShowcase Powered By <a href="https://sysborg.com.br" target="_blank" title="https://sysborg.com.br">
+                            <img src="{{asset('images/poweredby.png')}}" alt="Powered By Sysborg">
+                        </a> <span style="margin-left:10px; margin-right:10px;">|</span> <a href="https://andersonarruda.com.br" target="_blank" title="https://andersonarruda.com.br">
+                            <img src="{{asset('images/poweredby2.png')}}" alt="Powered By Anderson Arruda">
+                        </a>
+                    </div>
+                </div>
+            </footer>
         </div>
 
         <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
