4e89af22cbe24cd1daa04bd366344703ba3b4f37
Anderson ARruda <andmarruda@gmail.com>
2022-07-23 23:01:32 -0300

    preparing pattern for admin templates

--- database/migrations/2022_06_01_154900_create_categories_table.php
+++ database/migrations/2022_06_01_154900_create_categories_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestampsTz();
             $table->string('name', 100);
             $table->softDeletes();
-            
         });
     }
 
