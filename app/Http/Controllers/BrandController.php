08aa7bbb704611c554f52c4e2f9d7ac57e41a35d
Anderson ARruda <andmarruda@gmail.com>
2022-07-25 16:22:46 -0300

    Adding save on brand and type

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -8,6 +8,16 @@ use App\Models\Brand;
 class BrandController extends Controller
 {
     /**
+     * Error's messages
+     * @var     array
+     */
+    private array $error = [
+        'name.required' => 'O campo nome é obrigatório',
+        'name.min'  => 'O campo nome deve ter no mínimo 3 caracteres',
+        'name.max'  => 'O campo nome deve ter no máximo 50 caracteres'
+    ];
+
+    /**
      * Returns the view of brand form inside the admin
      * @version         1.0.0
      * @author          Anderson Arruda < andmarruda@gmail.com >
@@ -18,5 +28,26 @@ class BrandController extends Controller
     {
        $brand = is_null($id) ? NULL : Brand::withTrashed()->find($id);
        return view('admin.brand', ['Brand' => $brand]);
+    }
+
+    /**
+     * Save brand
+     * @version        1.0.0
+     * @author         Anderson Arruda < andmarruda@gmail.com >
+     * @param          Request $request
+     * @return         \Illuminate\Http\RedirectResponse
+     */
+    public function saveBrand(Request $request) : \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:50'
+        ], $this->error);
+
+        $brand = is_null($request->input('id')) ? new Brand() : Brand::withTrashed()->find($request->input('id'));
+        $brand->fill([
+            'name' => $request->input('name')
+        ]);
+        $saved = $brand->save();
+        return redirect()->route('brand')->with('saved', $saved);
     }
 }
--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -8,6 +8,16 @@ use App\Models\Type;
 class TypeController extends Controller
 {
     /**
+     * Error's messages
+     * @var     array
+     */
+    private array $error = [
+        'name.required' => 'O campo nome é obrigatório',
+        'name.min'  => 'O campo nome deve ter no mínimo 3 caracteres',
+        'name.max'  => 'O campo nome deve ter no máximo 50 caracteres'
+    ];
+
+    /**
      * Returns the view of type form inside the admin
      * @version         1.0.0
      * @author          Anderson Arruda < andmarruda@gmail.com >
@@ -18,5 +28,26 @@ class TypeController extends Controller
     {
        $type = is_null($id) ? NULL : Type::withTrashed()->find($id);
        return view('admin.type', ['Type' => $type]);
+    }
+
+    /**
+     * Save type
+     * @version        1.0.0
+     * @author         Anderson Arruda < andmarruda@gmail.com >
+     * @param          Request $request
+     * @return         \Illuminate\Http\RedirectResponse
+     */
+    public function saveType(Request $request) : \Illuminate\Http\RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:50'
+        ], $this->error);
+
+        $type = is_null($request->input('id')) ? new Type() : Type::withTrashed()->find($request->input('id'));
+        $type->fill([
+            'name' => $request->input('name')
+        ]);
+        $saved = $type->save();
+        return redirect()->route('type')->with('saved', $saved);
     }
 }
