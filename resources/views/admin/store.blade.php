e75e50e65fd55c82d826730fe4e0ec4ce8a4ca74
Anderson ARruda <andmarruda@gmail.com>
2022-07-26 21:48:07 -0300

    Store template ready

--- resources/views/admin/store.blade.php
+++ resources/views/admin/store.blade.php
@@ -12,21 +12,59 @@
         </ol>
     </nav>
 
-    $table->string('name');
-            $table->string('address');
-            $table->string('address_number');
-            $table->string('address_complement')->nullable();
-            $table->string('address_neighborhood');
-            $table->string('address_city');
-            $table->string('address_state');
-            $table->string('address_zipcode');
-            $table->string('address_country');
-            $table->string('google_maps_embeded');
-            $table->string('phone');
-
     <div class="mb-3">
         <label for="name" class="form-label">Nome da loja</label>
-        <input type="text" minlength="3" maxlength="50" class="form-control" id="name" name="name" placeholder="Nome da marca" required value="{{$Brand->name ?? ''}}">
+        <input type="text" minlength="3" maxlength="50" class="form-control" id="name" name="name" placeholder="Nome da loja" required value="{{$Store->name ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address" class="form-label">Endereço</label>
+        <input type="text" minlength="3" maxlength="60" class="form-control" id="address" name="address" placeholder="Endereço" required value="{{$Store->address ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_number" class="form-label">Número</label>
+        <input type="text" minlength="3" maxlength="60" class="form-control" id="address_number" name="address_number" placeholder="Número" required value="{{$Store->address_number ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_complement" class="form-label">Complemento</label>
+        <input type="text" class="form-control" id="address_complement" name="address_complement" placeholder="Complemento" required value="{{$Store->address_complement ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_neighborhood" class="form-label">Bairro</label>
+        <input type="text" minlength="3" maxlength="60" class="form-control" id="address_neighborhood" name="address_neighborhood" placeholder="Bairro" required value="{{$Store->address_neighborhood ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_city" class="form-label">Cidade</label>
+        <input type="text" minlength="3" maxlength="60" class="form-control" id="address_city" name="address_city" placeholder="Cidade" required value="{{$Store->address_city ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_state" class="form-label">UF</label>
+        <input type="text" minlength="2" maxlength="3" class="form-control" id="address_state" name="address_state" placeholder="UF" required value="{{$Store->address_state ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_zipcode" class="form-label">CEP</label>
+        <input type="text" maxlength="10" class="form-control" id="address_zipcode" name="address_zipcode" placeholder="CEP" required value="{{$Store->address_zipcode ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="phone" class="form-label">Telefone</label>
+        <input type="text" minlength="15" maxlength="20" class="form-control" id="phone" name="phone" placeholder="Número" required value="{{$Store->phone ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="address_country" class="form-label">País</label>
+        <input type="text" maxlength="30" class="form-control" id="address_country" name="address_country" placeholder="País" required value="{{$Store->address_country ?? ''}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="google_maps_embeded" class="form-label">Google Maps</label>
+        <input type="text" minlength="5" class="form-control" id="google_maps_embeded" name="google_maps_embeded" placeholder="Google Maps" required value="{{$Store->google_maps_embeded ?? ''}}">
     </div>
 
     @include('template.includes.alert-error')
@@ -43,10 +81,10 @@
         <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#searchModal">Pesquisar</button>
 
         @if(!is_null($Store))
-        @include('template.includes.disable-enable', ['enabled' => is_null($Store->deleted_at), 'route' => route('brand.delete'), 'id' => $Store->id, 'token' => csrf_token()])
+        @include('template.includes.disable-enable', ['enabled' => is_null($Store->deleted_at), 'route' => route('store.delete'), 'id' => $Store->id, 'token' => csrf_token()])
         @endif
     </div>
 </form>
 
-@include('template.includes.search-modal', ['modalTitle' => 'Pesquisar marca', 'placeholder' => 'Marca', 'route' => route('brand.search'), 'loadRoute' => route('brand'), 'ths' => ['#', 'Marca', 'Ativa?']])
+@include('template.includes.search-modal', ['modalTitle' => 'Pesquisar marca', 'placeholder' => 'Marca', 'route' => route('store.search'), 'loadRoute' => route('store'), 'ths' => ['#', 'Marca', 'Ativa?']])
 @endsection
