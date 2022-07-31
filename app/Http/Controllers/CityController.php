90451aeb9f94ba8da1e99b799a036fe33863807c
Anderson ARruda <andmarruda@gmail.com>
2022-07-30 21:43:43 -0300

    returning all cities from some state into CityController

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    /**
+     * Getting all cities from a state ordered by city name and filtered by state id
+     * @version         1.0.0
+     * @author          Anderson Arruda < andmarruda@gmail.com >
+     * @param           int $state_id
+     * @return          \Illuminate\Database\Eloquent\Collection
+     */
+    public function allCity(int $state_id) : \Illuminate\Database\Eloquent\Collection
+    {
+        return \App\Models\City::where('state_id', $state_id)->orderBy('city_name', 'asc')->get();
+    }
 }
