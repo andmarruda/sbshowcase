836651988dff82d9e66b37487649430840f7bc3e
Anderson ARruda <andmarruda@gmail.com>
2022-08-11 23:06:44 -0300

    fixed middleware for customer area

--- app/Http/Middleware/SBCustomerAuth.php
+++ app/Http/Middleware/SBCustomerAuth.php
@@ -6,6 +6,9 @@ use Closure;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CustomerAreaController;
 
+if(session_status() != PHP_SESSION_ACTIVE)
+    session_start();
+
 class SBCustomerAuth
 {
     /**
