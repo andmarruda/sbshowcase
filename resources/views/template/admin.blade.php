b91d67a0e71b77474d2f2eb0ff34161ec87b3508
Anderson ARruda <andmarruda@gmail.com>
2022-08-17 21:36:44 -0300

    improving admin template

--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">SBShowcase</a>
+                        <a class="navbar-brand" href="{{route('dashboard')}}">SBShowcase</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#admin-navbar" aria-controls="admin-navbar" aria-expanded="false">
                             <span class="navbar-toggler-icon"></span>
                         </button>
